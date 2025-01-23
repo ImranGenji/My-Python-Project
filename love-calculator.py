334548c6cd5519cc69de5944b06241ecbdf2aede
imran <imrangenji05@gmail.com>
2025-01-24 01:41:30 +0800

    name change love calculator
    name changed

--- love-calculator.py
+++ love-calculator.py
@@ -20,23 +20,23 @@ heading = Label(root, text='Love Calculator????')
 heading.pack()
 
 # Slot/input for the first name
-slot1 = Label(root, text="Nama Kamu:")
+slot1 = Label(root, text="Imran:")
 slot1.pack()
 name1 = Entry(root, border=5)
 name1.pack()
 
 # Slot/input for the partner name
-slot2 = Label(root, text="Nama crush/pacar kamu:")
+slot2 = Label(root, text="Angel:")
 slot2.pack()
 name2 = Entry(root, border=5)
 name2.pack()
 
-bt = Button(root, text="Hitung", height=1,
+bt = Button(root, text="Calculate", height=1,
             width=7, command=calculate_love)
 bt.pack()
 
 # Text on result slot
-result = Label(root, text='Persentase kecocokan:')
+result = Label(root, text='Matching Percentage:')
 result.pack()
 
 # Starting the GUI
